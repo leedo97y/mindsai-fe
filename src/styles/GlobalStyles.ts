920ff36956808c4cfe37f7ba53513c10bf953b05
leedo97y <dlehdus1433@gmail.com>
2024-10-25 04:37:32 +0900

    style: #1 globalstyle 추가

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -14,4 +14,18 @@ export const globalStyles = css`
     padding: 0;
     background-color: #fff;
   }
+
+  /* 스크롤바의 폭 너비 */
+  &::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: rgba(0, 0, 0, 0.1); /* 스크롤바 색상 */
+    border-radius: 10px; /* 스크롤바 둥근 테두리 */
+  }
+
+  &::-webkit-scrollbar-track {
+    background: rgba(255, 255, 255, 0); /*스크롤바 뒷 배경 색상*/
+  }
 `;
